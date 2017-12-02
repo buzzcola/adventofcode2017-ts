b12f20920931c87188d928175ac9c39f4b6c0a82
James Orr <wwwmailacct@gmail.com>
2017-12-02 10:03:54 -0500

    Day 2!

--- Day01/day01.ts
+++ Day01/day01.ts
@@ -1,18 +1,20 @@
-const BIG_PROBLEM: string = '525544371475555531777715244182678432191828'; // truncated
+namespace Day01 {
+    const BIG_PROBLEM: string = '525544371475555531777715244182678432191828'; // truncated
 
-function solve(input: string, offset:number) {
-    let digits: number[] = Array.from(input).map(c => +c);
+    function solve(input: string, offset: number) {
+        let digits: number[] = Array.from(input).map(c => +c);
 
-    return digits.map((v, i) => i) // i.e. range(0, digits.length);
-        .filter(i => digits[i] === digits[(i + offset) % digits.length])
-        .map(i => digits[i])
-        .reduce((a, c) => a + c, 0);
+        return digits.map((v, i) => i) // i.e. range(0, digits.length);
+            .filter(i => digits[i] === digits[(i + offset) % digits.length])
+            .map(i => digits[i])
+            .reduce((a, c) => a + c, 0);
+    }
+
+    ["1122", "1111", "1234", "91212129", BIG_PROBLEM].forEach(problem => {
+        console.log(solve(problem, 1))
+    });
+
+    ["1212", "1221", "123425", "123123", "12131415", BIG_PROBLEM].forEach(problem => {
+        console.log(solve(problem, problem.length / 2))
+    });
 }
-
-["1122", "1111", "1234", "91212129", BIG_PROBLEM].forEach(problem => {
-    console.log(solve(problem, 1))
-});
-
-["1212", "1221", "123425", "123123", "12131415", BIG_PROBLEM].forEach(problem => {
-    console.log(solve(problem, problem.length / 2))
-});
