477e231a0e65bc0323a505c1e7e9d8f7dc43169e
buzzcola <orr.james@gmail.com>
2017-12-06 13:18:00 -0500

    finagle 2.0

--- Day06/day06.ts
+++ Day06/day06.ts
@@ -2,21 +2,21 @@ namespace Day06 {
 
     let banks: number[] = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14];
 
-    function solve(input: number[]): { steps: number, banks: number[] } {        
-        let makeKey = (b:number[]) => b.map(x => '' + x).join('\t');
+    function solve(input: number[]): { steps: number, banks: number[] } {
+        let makeKey = (b: number[]) => b.map(x => '' + x).join('\t');
         let banks = [...input];
         let states = new Set<string>();
         let key = makeKey(banks);
         let steps = 0;
 
-        while(!states.has(key)){        
+        while (!states.has(key)) {
             states.add(key);
             redistribute(banks);
             key = makeKey(banks);
             steps++;
         }
 
-        return { steps: steps, banks: key.split('\t').map(x => +x) };
+        return { steps: steps, banks: banks };
     }
 
     function redistribute(banks: number[]) {
