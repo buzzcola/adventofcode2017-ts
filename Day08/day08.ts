820a821ca9a8078d90b28f15c26959f008cd63dd
James Orr <jorr@questica.com>
2017-12-08 15:06:23 -0500

    ...

--- Day08/day08.ts
+++ Day08/day08.ts
@@ -5,20 +5,18 @@ namespace Day08 {
     function solve(input: string): { maxFinish: number, maxGlobal: number } {
         let registers: { [name: string]: number } = {};
         let globalMax = 0;
-        input.split('\n')
-            .forEach(line => {
-                let values = /^(\S+)\s+(\S+)\s+(-?\d+)\s+if\s+((\S+)\s\S+\s-?\d+)$/.exec(line);
-                let [target, op, amount, evalText, evalTarget] = values.slice(1);
-
-                // register new registers.
-                if(!registers.hasOwnProperty(target)) registers[target] = 0;
-                if(!registers.hasOwnProperty(evalTarget)) registers[evalTarget] = 0;
-                
-                if (eval('registers.' + evalText)) {
-                    registers[target] += +amount * (op === 'dec' ? -1 : 1);
-                    globalMax = Math.max(globalMax, registers[target]);
-                }
-            });
+        input.split('\n').forEach(line => {
+            let values = /^(\S+)\s+(\S+)\s+(-?\d+)\s+if\s+((\S+)\s\S+\s-?\d+)$/.exec(line);
+            let [target, op, amount, evalText, evalTarget] = values.slice(1);
+            
+            if (!registers.hasOwnProperty(target)) registers[target] = 0;
+            if (!registers.hasOwnProperty(evalTarget)) registers[evalTarget] = 0;
+            
+            if (eval('registers.' + evalText)) {
+                registers[target] += +amount * (op === 'dec' ? -1 : 1);
+                globalMax = Math.max(globalMax, registers[target]);
+            }
+        });
 
         return {
             maxFinish: Math.max(...Object.keys(registers).map(k => registers[k])),
