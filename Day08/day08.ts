1eb044424d1dde96ad02f64171a7a28aecd10ddb
James Orr <jorr@questica.com>
2017-12-08 14:58:20 -0500

    multi-assignment is just as pretty as the class was, and much shorter.

--- Day08/day08.ts
+++ Day08/day08.ts
@@ -2,33 +2,22 @@
 
 namespace Day08 {
 
-    class Instruction {
-        constructor(
-            public target: string,
-            public operation: string,
-            public amount: number,
-            public evalText: string,
-            public evalTarget: string) { }
-
-        static fromString(input: string): Instruction {
-            let data = /^(\S+)\s+(\S+)\s+(-?\d+)\s+if\s+((\S+)\s\S+\s-?\d+)$/.exec(input);
-            return new Instruction(data[1], data[2], +data[3], data[4], data[5]);
-        }
-    }
-
     function solve(input: string): { maxFinish: number, maxGlobal: number } {
         let registers: { [name: string]: number } = {};
         let globalMax = 0;
         input.split('\n')
-            .map(x => Instruction.fromString(x))
-            .forEach(i => {
-                // register new registers.
-                if (!registers.hasOwnProperty(i.evalTarget)) registers[i.evalTarget] = 0;
-                if (!registers.hasOwnProperty(i.target)) registers[i.target] = 0;
+            .forEach(line => {
+                let values = /^(\S+)\s+(\S+)\s+(-?\d+)\s+if\s+((\S+)\s\S+\s-?\d+)$/.exec(line);
+                let [target, op, amount, evalText, evalTarget] = values.slice(1);
 
-                if (eval('registers.' + i.evalText)) {
-                    registers[i.target] += i.amount * (i.operation === 'dec' ? -1 : 1);
-                    globalMax = Math.max(globalMax, registers[i.target]);
+                // register new registers.
+                [target, evalTarget]
+                    .filter(x => !registers.hasOwnProperty(x))
+                    .forEach(x => registers[x] = 0);
+                
+                if (eval('registers.' + evalText)) {
+                    registers[target] += +amount * (op === 'dec' ? -1 : 1);
+                    globalMax = Math.max(globalMax, registers[target]);
                 }
             });
 
