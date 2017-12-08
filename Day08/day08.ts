0d99b5e0b4a04e47a48c3f0c7c5cc71197336079
James Orr <jorr@questica.com>
2017-12-08 08:35:21 -0500

    ...

--- Day08/day08.ts
+++ Day08/day08.ts
@@ -3,14 +3,14 @@
 namespace Day08 {
 
     class Instruction {
-        constructor(        
+        constructor(
             public target: string,
             public operation: string,
             public amount: number,
             public evalText: string,
-            public evalTarget: string){}        
+            public evalTarget: string) { }
 
-        static fromString(input:string):Instruction {
+        static fromString(input: string): Instruction {
             let data = /^(\S+)\s+(\S+)\s+(-?\d+)\s+if\s+((\S+)\s\S+\s-?\d+)$/.exec(input);
             return new Instruction(data[1], data[2], +data[3], data[4], data[5]);
         }
@@ -18,21 +18,20 @@ namespace Day08 {
 
     function solve(input: string): { maxFinish: number, maxGlobal: number } {
         let registers: { [name: string]: number } = {};
-        let instructions = input.split('\n')
-            .map(x => Instruction.fromString(x));
-
         let globalMax = Number.MIN_SAFE_INTEGER;
-        instructions.forEach(instruction => {
-            // register new registers.
-            if(!registers.hasOwnProperty(instruction.evalTarget)) registers[instruction.evalTarget] = 0;
-            if(!registers.hasOwnProperty(instruction.target)) registers[instruction.target] = 0;
+        input.split('\n')
+            .map(x => Instruction.fromString(x))
+            .forEach(instruction => {
+                // register new registers.
+                if (!registers.hasOwnProperty(instruction.evalTarget)) registers[instruction.evalTarget] = 0;
+                if (!registers.hasOwnProperty(instruction.target)) registers[instruction.target] = 0;
 
-            if (eval('registers.' + instruction.evalText) as boolean) {
-                let amount = instruction.amount * (instruction.operation === 'dec' ? -1 : 1);
-                registers[instruction.target] += amount;
-                globalMax = Math.max(globalMax, registers[instruction.target]);
-            }
-        });
+                if (eval('registers.' + instruction.evalText) as boolean) {
+                    let amount = instruction.amount * (instruction.operation === 'dec' ? -1 : 1);
+                    registers[instruction.target] += amount;
+                    globalMax = Math.max(globalMax, registers[instruction.target]);
+                }
+            });
 
         return {
             maxFinish: Math.max(...Object.keys(registers).map(k => registers[k])),
