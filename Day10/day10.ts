64be3fd59e1b9931930173382d75409d0cb56280
buzzcola <orr.james@gmail.com>
2017-12-10 02:36:52 -0500

    can't sleep

---
+++ Day10/day10.ts
@@ -0,0 +1,61 @@
+/// <reference path="input.ts" />
+namespace Day10 {
+
+    function solve1(listSize: number, lengths: number[], rounds: number = 1): number[] {
+        let list = range(listSize);
+        let position = 0;
+        let skipSize = 0;
+        for (let _ of range(rounds)) {
+            for (let length of lengths) {
+                let indexes = range(length).map(i => (position + i) % list.length);
+                let segment = indexes.map(i => list[i]);
+                segment.reverse();
+                indexes.forEach(i => list[i] = segment.shift());
+                position = (position + length + skipSize++) % list.length;
+            }
+        }
+
+        return list;
+    }
+
+    function solve2(listSize: number, input: string) {
+        let lengths = [...input]
+            .map(i => i.charCodeAt(0))
+            .concat(17, 31, 73, 47, 23);
+
+        let sparseHash = solve1(listSize, lengths, 64);
+        return range(listSize / 16)
+            .map(i => sparseHash.slice(i * 16, (i + 1) * 16))
+            .map(chunk => chunk.reduce((x, y) => x ^ y))
+            .map(x => x.toString(16))
+            .map(x => x.length == 2 ? x : '0' + x)
+            .join('');
+    }
+
+    function range(size: number): number[] {
+        return [...Array(size).keys()]
+    }
+
+
+    let test1 = solve1(5, SAMPLE_PROBLEM_1);
+    let pass1 = test1[0] * test1[1] === SAMPLE_ANSWER_1;
+    console.log(`Part 1 Test -  ${pass1 ? 'pass' : 'fail'}: expected:${SAMPLE_ANSWER_1} actual:${test1[0] * test1[1]}`);
+    if (pass1) {
+        let problemArray = BIG_PROBLEM.split(',').map(x => +x);
+        let answer = solve1(256, problemArray);
+        console.log(`Part 1 Answer: ${answer[0] * answer[1]}`);
+    }
+
+    let pass2 = true;
+    SAMPLE_PROBLEMS_2.forEach(problem => {
+        let test2 = solve2(256, problem[0]);
+        let pass = test2 === problem[1];
+        console.log(`Part 2 Test -  ${pass ? 'pass' : 'fail'}: expected:${problem[1]} actual:${test2}`);
+        pass2 = pass2 && pass;
+    });
+
+    if (pass2) {
+        console.log(`Part 2 Answer: ${solve2(256, BIG_PROBLEM)}`);
+    }
+}
+
---
+++ Day10/input.ts
@@ -0,0 +1,13 @@
+namespace Day10 {
+
+    export const SAMPLE_PROBLEM_1 = [3, 4, 1, 5];
+    export const SAMPLE_ANSWER_1 = 12;    
+    export const SAMPLE_PROBLEMS_2 = [
+        ['', 'a2582a3a0e66e6e86e3812dcb672a272'],
+        ['AoC 2017', '33efeb34ea91902bb2f59c9920caa6cd'],
+        ['1,2,3', '3efbe78a8d82f29979031a4aa0b16a9d'],
+        ['1,2,4', '63960835bcdc130f0b66d7ff4f6a5a8e']
+    ];
+        
+    export const BIG_PROBLEM = '227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144';
+}
