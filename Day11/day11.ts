83428d247283573babd060be056b319b143bed4a
James Orr <jorr@questica.com>
2017-12-11 10:24:01 -0500

    day 11

--- Day11/day11.ts
+++ Day11/day11.ts
@@ -1,38 +1,33 @@
 /// <reference path="input.ts" />
 namespace Day11 {
 
+    type Point = [number, number];
+
     // using the cube coordinate system from https://www.redblobgames.com/grids/hexagons/
-    let directions: {[direction:string]:number[]} = {
-        'n':[0,-1],
-        'ne':[1,-1],
-        'se':[1, 0],
-        's':[0,1],
-        'sw':[-1,1],
-        'nw':[-1,0]
+    let directions: { [direction: string]: Point } = {
+        'n': [0, -1],
+        'ne': [1, -1],
+        'se': [1, 0],
+        's': [0, 1],
+        'sw': [-1, 1],
+        'nw': [-1, 0]
     }
 
-    function move(position:number[], direction:string):number[]{
-        return [
-            position[0] + directions[direction][0],
-            position[1] + directions[direction][1]
-         ];
-    }
-
-    function getDistance(position1:number[], position2:number[] = [0,0]):number{
+    function getDistance(position1: Point, position2: Point = [0, 0]): number {
         return Math.max(
-            Math.abs(position1[0] - position2[0]), 
-            Math.abs(position1[1] - position2[1]), 
-            Math.abs(
-                (position1[0] + position1[1]) - (position2[0] + position2[1])
-            ));
+            Math.abs(position1[0] - position2[0]),
+            Math.abs(position1[1] - position2[1]),
+            Math.abs((position1[0] + position1[1]) - (position2[0] + position2[1]))
+        );
     }
 
-    function solve1(input:string):{finish:number, max:number} {
+    function solve(input: string): { finish: number, max: number } {
         let moves = input.split(',');
-        let position = [0,0];
+        let position: Point = [0, 0];
         let max = 0;
-        for(let x of moves) {
-            position = move(position, x);        
+        for (let move of moves) {
+            position[0] += directions[move][0],
+            position[1] += directions[move][1]
             max = Math.max(max, getDistance(position));
         }
         return {
@@ -41,16 +36,16 @@ namespace Day11 {
         }
     }
 
-    let pass1 = true;
+    let pass = true;
     console.log('Part 1 Test')
-    for(let problem of SAMPLE_PROBLEMS_1){
-        let test = solve1(problem[0]);
+    for (let problem of SAMPLE_PROBLEMS_1) {
+        let test = solve(problem[0]);
         let pass = test.finish === problem[1];
-        console.log(`\t${pass ? 'pass' : 'fail'}: expected:${problem[1]} actual:${test}`);
-        pass1 = pass1 && pass;
+        console.log(`\t${pass ? 'pass' : 'fail'}: expected:${problem[1]} actual:${test.finish}`);
+        pass = pass && pass;
     }
-    if(pass1){
-        console.log(`Part 1 Answer: ${solve1(BIG_PROBLEM).finish}`);
-        console.log(`Part 1 Answer: ${solve1(BIG_PROBLEM).max}`);
+    if (pass) {
+        console.log(`Part 1 Answer: ${solve(BIG_PROBLEM).finish}`);
+        console.log(`Part 1 Answer: ${solve(BIG_PROBLEM).max}`);
     }
 }
