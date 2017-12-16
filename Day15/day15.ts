bf4b18ed38638bc50584afe13b3f9f3e729cbd95
buzzcola <orr.james@gmail.com>
2017-12-15 19:25:05 -0500

    day 15

--- Day15/day15.ts
+++ Day15/day15.ts
@@ -2,34 +2,45 @@
 
 namespace Day15 {
 
-    const SAMPLE_SEED_A = 65;
-    const SAMPLE_SEED_B = 8921;
-    const SAMPLE_ANSWER_1 = 588;
-    const SEED_A = 883;
-    const SEED_B = 879;
-    const FACTOR_A = 16807;
-    const FACTOR_B = 48271;
-    const BIT_MASK = 65535;
-
-    function makeGenerator(factor:number, seed:number){
-        let lastValue = seed;
-        const divisor = 2147483647;
+    function makeGenerator(name: string, seed: number, mod:number = 1) {
+        let factor = name.toUpperCase() == 'A' ? 16807 : 48271;
+        let value = seed;
         return () => {
-            lastValue = (lastValue * factor) % divisor;             
-            return lastValue & BIT_MASK;
+            do {
+                value = (value * factor) % 2147483647;
+            } while(value % mod != 0);       
+            return value & 65535;
         }
     }
 
-    function solve1(seed_a:number, seed_b:number){
-        let genA = makeGenerator(FACTOR_A, seed_a);
-        let genB = makeGenerator(FACTOR_B, seed_b);
-        
+    function solve1(seed_a: number, seed_b: number) {
+        let genA = makeGenerator('A', seed_a);
+        let genB = makeGenerator('B', seed_b);
         let matches = 0;
-        for(let i=0; i < 40000000; i++) {
-            if(genA() == genB()) matches++;
+                
+        for (let i = 0; i < 40000000; i++) {
+            if (genA() == genB()) {
+                matches++;
+            }
         }
+
         return matches;
     }
+
+    function solve2(seed_a: number, seed_b: number) {
+        let genA = makeGenerator('A', seed_a, 4);
+        let genB = makeGenerator('B', seed_b, 8);
+        let matches = 0;
+        
+        for (let i = 0; i < 5000000; i++) {
+            if (genA() == genB()) {
+                matches++;
+            }
+        }
+
+        return matches;
+    }
+
 
     let test1 = solve1(SAMPLE_SEED_A, SAMPLE_SEED_B);
     let pass1 = test1 === SAMPLE_ANSWER_1;
@@ -37,12 +48,11 @@ namespace Day15 {
     if (pass1) {
         console.log(`Part 1 Answer: ${solve1(SEED_A, SEED_B)}`);
     }
-/*
-    let test2 = solve2(SAMPLE_PROBLEM);
+
+    let test2 = solve2(SAMPLE_SEED_A, SAMPLE_SEED_B);
     let pass2 = test2 === SAMPLE_ANSWER_2;
     console.log(`Part 2 Test -  ${pass2 ? 'pass' : 'fail'}: expected:${SAMPLE_ANSWER_2} actual:${test2}`);
     if (pass2) {
-        console.log(`Part 2 Answer: ${solve2(BIG_PROBLEM)}`);
+        console.log(`Part 2 Answer: ${solve2(SEED_A, SEED_B)}`);
     }
-    */
 }
--- Day15/input.ts
+++ Day15/input.ts
@@ -1,3 +1,8 @@
 namespace Day15 {
-    
+    export const SAMPLE_SEED_A = 65;
+    export const SAMPLE_SEED_B = 8921;
+    export const SAMPLE_ANSWER_1 = 588;
+    export const SAMPLE_ANSWER_2 = 309;
+    export const SEED_A = 883;
+    export const SEED_B = 879;
 }
