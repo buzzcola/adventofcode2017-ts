fe953ca27004abc354dd887f4819470ec0adb5c7
James Orr <wwwmailacct@gmail.com>
2017-12-20 09:09:12 -0500

    prettier

--- Day20/Particle.ts
+++ Day20/Particle.ts
@@ -1,33 +1,34 @@
 namespace Day20 {
 
     type Point = { [dimension: string]: number, x: number, y: number, z: number }
+    const dimensions = ['x', 'y', 'z'];
 
     export class Particle {
-        constructor(public id:number, public position: Point, public velocity: Point, public acceleration: Point) { }
+        constructor(public id: number, public position: Point, public velocity: Point, public acceleration: Point) { }
 
         tick() {
-            for (let d of ['x', 'y', 'z']) {
+            for (let d of dimensions) {
                 this.velocity[d] += this.acceleration[d];
                 this.position[d] += this.velocity[d];
             }
         }
 
         get positionString() {
-            return ['x','y','z'].map(d => this.position[d]).join('-');
+            return dimensions.map(d => this.position[d]).join('-');
         }
 
         get distanceToOrigin() {
             let distance = 0;
-            for(let d of ['x','y','z']){
+            for (let d of dimensions) {
                 distance += Math.abs(this.position[d]);
             }
             return distance;
         }
 
-        static fromString(id:number, input: string): Particle {
+        static fromString(id: number, input: string): Particle {
             let rx = /\w=<[ ]?(-?\d+),([- ]?\d+),([- ]?\d+)>/g;
             let points: Point[] = [];
-            for (let i = 0; i < 3; i ++) {
+            for (let i = 0; i < 3; i++) {
                 let data = rx.exec(input);
                 points.push({ x: +data[1], y: +data[2], z: +data[3] });
             }
