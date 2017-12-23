b33a9733bd22def1b1754e73a63e6585afe21f47
James Orr <wwwmailacct@gmail.com>
2017-12-23 09:58:05 -0500

    day 23 wip

--- Day23/Program.ts
+++ Day23/Program.ts
@@ -1,41 +1,41 @@
-namespace Day18 {
+namespace Day23 {
 
     export class Program {
 
-        constructor(id: number, input: string) {
+        constructor(input: string, initialA: number = 0) {
             this.instructions = input.split('\n').map(d => ({
                 "name": d.substring(0, 3),
                 "args": d.substring(4).split(' ')
             }));
 
-            this.registers['p'] = id;
-            this.id = id;
+            'abcdefgh'.split('').forEach(c => this.registers[c] = 0);
+            this.registers['a'] = initialA;
         }
 
         instructions: { name: string, args: string[] }[] = [];
         registers: { [name: string]: number } = {};
-        queue: number[] = [];
         position = 0;
-        id: number;
-        sent = 0;
-        friend: Program;
 
         commands: { [name: string]: Function } = {
             "set": (a: string, b: string) => { this.registers[a] = this.parse(b); this.position++; },
-            "mul": (a: string, b: string) => { this.registers[a] *= this.parse(b); this.position++; },
-            "add": (a: string, b: string) => { this.registers[a] += this.parse(b); this.position++; },
             "sub": (a: string, b: string) => { this.registers[a] -= this.parse(b); this.position++; },
-            "mod": (a: string, b: string) => { this.registers[a] = this.registers[a] % this.parse(b); this.position++; },
-            "jgz": (a: string, b: string) => { this.position += this.parse(a) > 0 ? this.parse(b) : 1; },
+            "mul": (a: string, b: string) => { this.registers[a] *= this.parse(b); this.position++; },
             "jnz": (a: string, b: string) => { this.position += this.parse(a) != 0 ? this.parse(b) : 1; },
-            "snd": (a: string) => { this.friend.queue.push(this.parse(a)); this.position++; this.sent++; },
-            "rcv": (a: string) => { if (this.queue.length > 0) { this.registers[a] = this.queue.shift(); this.position++; } }
         }
 
         execute() {
             let instruction = this.instructions[this.position];
             this.commands[instruction.name](...instruction.args);
             return instruction.name;
+        }
+
+        get next() {
+            let i = this.instructions[this.position];
+            if(i) {
+                return i.name + ' ' + i.args;
+            } else {
+                return '<none>';
+            }
         }
 
         parse(arg: string) {
@@ -45,10 +45,5 @@ namespace Day18 {
         get finished() {
             return this.position < 0 || this.position >= this.instructions.length;
         }
-
-        get waiting() {
-            return (this.instructions[this.position].name == 'rcv' && this.queue.length == 0);
-        }
     }
-
 }
--- Day23/day23.ts
+++ Day23/day23.ts
@@ -1,27 +1,69 @@
 /// <reference path="input.ts" />
-/// <reference path="../Day18/program.ts" />
+/// <reference path="Program.ts" />
 
 namespace Day23 {
 
-    function solve1(input:string){
-        let p = new Day18.Program(0, BIG_PROBLEM);
+    function solve1(input: string) {
+        let p = new Program(BIG_PROBLEM);
         let mulCount = 0;
-        while(!p.finished){
-            if(p.execute() === 'mul'){
+        while (!p.finished) {
+            if (p.execute() === 'mul') {
                 mulCount++;
             }
         }
-        return mulCount;        
+        return mulCount;
+    }
+
+    function solve2(input: string) {
+        let [a, b, c, d, e, f, g, h] = [1, 105700, 122700, 0, 0, 0, 0, 0]; // state as of instruction 09        
+        let fn: { [line: number]: Function } = {
+            9: () => { [f, d, e, g] = [1, 2, 2, 4 - b]; return (g != 0) ? 17 : 16; },
+            11: () => { e = 2; return 12; },
+            12: () => { g = d; g *= e; g -= b; return (g != 0) ? 17 : 16; },
+            16: () => { f = 0; return 17; },
+            17: () => { e -= 1; g = e; g -= b; return (g != 0) ? 12 : 21; },
+            21: () => {
+                d -= 1; g = d; g -= b;
+                console.log(`21: g = ${g}`);
+                return (g != 0) ? 11 : 25;
+            },
+            25: () => {
+                console.log(`25: f = ${f}, h = ${h}`);
+                if (f != 0) h -= 1;
+                return 27;
+            },
+            27: () => {
+                g = b;
+                g -= c;
+                if (g != 0) {
+                    b -= 17;
+                    console.log(`27: g = ${g}, h = ${h}`);
+                    return 9;
+                } else { return undefined; }
+            }
+        };
+
+        let position = 9;
+        while (position != undefined) {
+            position = fn[position]();
+        }
+
+        return h;
+    }
+
+    function solve3(input: string) {
+        let [a, b, c, d, e, f, g, h] = [1, 105700, 122700, 2, 2, 1, -105696, 0]; // state as of instruction 10
+        while (g != 0) {
+            g = 0;
+            h -= 1;
+            g = b - c;
+            if (g == 0) break;
+            b -= 17
+        }
+
+        return h;
     }
 
     console.log(`Part 1 Answer: ${solve1(BIG_PROBLEM)}`);
-
-    /*
-    let test2 = solve2(SAMPLE_PROBLEM, 10000000);
-    let pass2 = test2 === SAMPLE_ANSWER_2;
-    console.log(`Part 2 Test -  ${pass1 ? 'pass' : 'fail'}: expected:${SAMPLE_ANSWER_2} actual:${test2}`);
-    if (pass2) {
-        console.log(`Part 2 Answer: ${solve2(BIG_PROBLEM, 10000000)}`);
-    }
-    */
+    console.log(`Part 2 Answer: ${solve2(BIG_PROBLEM)}`);
 }
